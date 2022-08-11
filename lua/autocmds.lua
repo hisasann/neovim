6fa2c745929e3160765b363e06c6b9c4c721c659
hisamatsu yoshiyuki <hisasann25@gmail.com>
2022-08-11 14:20:08 +0900

    feat: add autocmds.lua

---
+++ lua/autocmds.lua
@@ -0,0 +1,14 @@
+local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+
+-- Remove whitespace on save
+autocmd("BufWritePre", {
+	pattern = "*",
+	command = ":%s/\\s\\+$//e",
+})
+
+-- Don't auto commenting new lines
+autocmd("BufEnter", {
+	pattern = "*",
+	command = "set fo-=c fo-=r fo-=o",
+})
