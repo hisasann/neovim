532a88e8678d6bbdac1c8d2cb94811cefe51ea39
hisamatsu yoshiyuki <hisasann25@gmail.com>
2022-08-11 14:19:57 +0900

    format

--- lua/base.lua
+++ lua/base.lua
@@ -1,14 +1,10 @@
 vim.cmd("autocmd!")
 
-vim.scriptencoding = 'utf-8'
+vim.scriptencoding = "utf-8"
 
 vim.wo.number = true
 
 -- Open hoge file
-vim.api.nvim_create_user_command(
-  'Hoge',
-  function(opts)
-    vim.cmd('e ' .. '~/_/hoge/hoge.markdown')
-  end,
-  {}
-)
+vim.api.nvim_create_user_command("Hoge", function(opts)
+	vim.cmd("e " .. "~/_/hoge/hoge.markdown")
+end, {})
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -19,69 +19,62 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
-keymap('n', '<C-h>', '<C-w>h', opts)
-keymap('n', '<C-j>', '<C-w>j', opts)
-keymap('n', '<C-k>', '<C-w>k', opts)
-keymap('n', '<C-l>', '<C-w>l', opts)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- New tab
-keymap('n', 'te', ':tabedit', opts)
+keymap("n", "te", ":tabedit", opts)
 -- 新しいタブを一番右に作る
-keymap('n', 'gn', ':tabnew<Return>', opts)
+keymap("n", "gn", ":tabnew<Return>", opts)
 -- move tab
-keymap('n', 'gh', 'gT', opts)
-keymap('n', 'gl', 'gt', opts)
+keymap("n", "gh", "gT", opts)
+keymap("n", "gl", "gt", opts)
 
 -- Split window
-keymap('n', 'ss', ':split<Return><C-w>w', opts)
-keymap('n', 'sv', ':vsplit<Return><C-w>w', opts)
+keymap("n", "ss", ":split<Return><C-w>w", opts)
+keymap("n", "sv", ":vsplit<Return><C-w>w", opts)
 
 -- Select all
-keymap('n', '<C-a>', 'gg<S-v>G', opts)
+keymap("n", "<C-a>", "gg<S-v>G", opts)
 
 -- Do not yank with x
-keymap('n', 'x', '"_x', opts)
+keymap("n", "x", '"_x', opts)
 
 -- Delete a word backwards
-keymap('n', 'dw', 'vb"_d', opts)
+keymap("n", "dw", 'vb"_d', opts)
 
 -- 行の端に行く
-keymap('n', '<Space>h', '^', opts)
-keymap('n', '<Space>l', '$', opts)
+keymap("n", "<Space>h", "^", opts)
+keymap("n", "<Space>l", "$", opts)
 
 -- ;でコマンド入力( ;と:を入れ替)
-keymap('n', ';', ':', opts)
+keymap("n", ";", ":", opts)
 
 -- 行末までのヤンクにする
-keymap('n', 'Y', 'y$', opts)
+keymap("n", "Y", "y$", opts)
 
 -- <Space>q で強制終了
-keymap('n', '<Space>q', ':<C-u>q!<Return>', opts)
+keymap("n", "<Space>q", ":<C-u>q!<Return>", opts)
 
 -- ESC*2 でハイライトやめる
-keymap('n', '<Esc><Esc>', ':<C-u>set nohlsearch<Return>', opts)
-
-
+keymap("n", "<Esc><Esc>", ":<C-u>set nohlsearch<Return>", opts)
 
 -- Insert --
--- Press jk fast to exit insert mode 
-keymap('i', 'jk', '<ESC>', opts)
+-- Press jk fast to exit insert mode
+keymap("i", "jk", "<ESC>", opts)
 
 -- コンマの後に自動的にスペースを挿入
-keymap('i', ',', ',<Space>', opts)
-
-
-
+keymap("i", ",", ",<Space>", opts)
 
 -- Visual --
 -- Stay in indent mode
-keymap('v', '<', '<gv', opts)
-keymap('v', '>', '>gv', opts)
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
 
 -- ビジュアルモード時vで行末まで選択
-keymap('v', 'v', '$h', opts)
+keymap("v", "v", "$h", opts)
 
 -- 0番レジスタを使いやすくした
-keymap('v', '<C-p>', '"0p', opts)
-
-
+keymap("v", "<C-p>", '"0p', opts)
