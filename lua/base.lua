faf114346a222c151d8bfc3776c5dc973257de67
hisamatsu yoshiyuki <hisasann25@gmail.com>
2022-08-11 16:19:34 +0900

    feat: add `Restore cursor location when file is opened`

--- lua/base.lua
+++ lua/base.lua
@@ -8,3 +8,11 @@ vim.wo.number = true
 vim.api.nvim_create_user_command("Hoge", function(opts)
 	vim.cmd("e " .. "~/_/hoge/hoge.markdown")
 end, {})
+
+-- Restore cursor location when file is opened
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+	pattern = { "*" },
+	callback = function()
+		vim.api.nvim_exec('silent! normal! g`"zv', false)
+	end,
+})
