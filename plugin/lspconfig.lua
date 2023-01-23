ed08bead8b2a13154c3ca263026dd713c8588a77
hisamatsu yoshiyuki <hisasann25@gmail.com>
2023-01-24 02:17:38 +0900

    feat: formatting_seq_sync() to format()
    via https://dev.classmethod.jp/articles/eetann-okorare-neovim/

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -25,7 +25,7 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = vim.api.nvim_create_augroup("Format", { clear = true }),
       buffer = bufnr,
-      callback = function() vim.lsp.buf.formatting_seq_sync() end
+      callback = function() vim.lsp.buf.format() end
     })
   end
 
