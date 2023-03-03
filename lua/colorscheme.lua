5eadc92c7b618b0bcc169e489a21908ddfa369c8
hisamatsu yoshiyuki <hisasann25@gmail.com>
2023-03-04 01:53:01 +0900

    feat: colorscheme tokyonight.nvim

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,6 +1,6 @@
 vim.cmd [[
 try
-  colorscheme nightfox
+  colorscheme tokyonight
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -199,6 +199,11 @@ _G.packer_plugins = {
     path = "/Users/hisasann/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["tokyonight.nvim"] = {
+    loaded = true,
+    path = "/Users/hisasann/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    url = "https://github.com/folke/tokyonight.nvim"
+  },
   ["zen-mode.nvim"] = {
     config = { "\27LJ\2\n:\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\rzen-mode\frequire\0" },
     loaded = true,
