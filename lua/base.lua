de4fbd559f105c6be12fa9cd9d054fef071c0310
hisamatsu yoshiyuki <hisasann25@gmail.com>
2023-08-10 21:15:08 +0900

    feat: add remove regex of return code

--- lua/base.lua
+++ lua/base.lua
@@ -6,5 +6,10 @@ vim.wo.number = true
 
 -- Open hoge file
 vim.api.nvim_create_user_command("Hoge", function(opts)
-	vim.cmd("e " .. "~/_/hoge/hoge.markdown")
+  vim.cmd("e " .. "~/_/hoge/hoge.markdown")
+end, {})
+
+-- remove return
+vim.api.nvim_create_user_command("NN", function(opts)
+  vim.cmd(":%s/\\%(.\\)\\@<!\\n//ig")
 end, {})
